1bc160a32accc0ac345663cd86bfe26b1ac222f9
Helen Dechat <hdechat@gmail.com>
2018-05-25 20:59:29 -0600

    Initial Commit

---
+++ 7-day-menu-planner/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      recipeCards: []
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
