0f47c4eab43e22278e97bc7f50ad554454d3abf0
Helen Dechat <hdechat@gmail.com>
2018-05-26 12:10:06 -0600

    Add setupTests with enzyme confgure and snapshot test

--- 7-day-menu-planner/src/App.js
+++ 7-day-menu-planner/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {paleo} from './mock-data.js'
-import {cleanData} from './helpers.js'
+import {cleanData} from './helpers/helpers.js'
 
 class App extends Component {
   constructor(props) {
@@ -16,7 +16,7 @@ class App extends Component {
     console.log(cleanData(paleo))
     return (
       <div className="App">
-        <select onChange={(event)=>{console.log(event.target.value)}}id='diet-plan'>
+        <select onChange={(event)=>{console.log(event.target.value)}} id='diet-plan'>
           <option value='paleo'>Paleo</option>
           <option value='vegetarian'>Vegetarian</option>
           <option value='vegan'>Vegan</option>
