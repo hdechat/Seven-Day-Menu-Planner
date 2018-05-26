cca4e4276b90ddfab6b0f981d8feef0eeb21f957
Helen Dechat <hdechat@gmail.com>
2018-05-26 14:19:52 -0600

    Add paleo reducers and tests

--- 7-day-menu-planner/src/helpers/helpers.js
+++ 7-day-menu-planner/src/helpers/helpers.js
@@ -1,6 +1,7 @@
 export const cleanData = (data) => {
   return data.hits.map(hit => {
     return {
+      tite: hit.recipe.label,
       image: hit.recipe.image,
       url: hit.recipe.url,
       ingredients: hit.recipe.ingredientLines
