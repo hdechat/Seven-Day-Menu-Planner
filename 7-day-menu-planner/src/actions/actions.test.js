06671dc273d03a3cdc36a641b7fcf776dd9a06f6
Helen Dechat <hdechat@gmail.com>
2018-05-26 13:21:37 -0600

    Add paleo is loading test and action

--- 7-day-menu-planner/src/actions/actions.test.js
+++ 7-day-menu-planner/src/actions/actions.test.js
@@ -1,16 +1,32 @@
 import * as actions from './index.js';
 import { cleanedPaleoRecipes } from '../mock-data.js';
 
-describe('Paleo Fetch Success', () => {
-  it('should return an array of cleaned recipe objects', () => {
-    const expected = {
-      type: 'FETCH_PALEO_SUCCESS',
-      recipes: cleanedPaleoRecipes
-    };
+describe('PALEO ACTIONS', () => {
+  describe('Paleo Fetch Success', () => {
+    it('should return an array of cleaned recipe objects', () => {
+      const expected = {
+        type: 'FETCH_PALEO_SUCCESS',
+        recipes: cleanedPaleoRecipes
+      };
 
-    const actual = actions.fetchPaleoSuccess(cleanedPaleoRecipes)
+      const actual = actions.fetchPaleoSuccess(cleanedPaleoRecipes)
 
-    expect(actual).toEqual(expected);
+      expect(actual).toEqual(expected);
+    });
   });
-  
+
+  describe('Paleo Is Loading', () => {
+    it('should return a boolean', () => {
+      const expected = {
+        type: 'PALEO_IS_LOADING',
+        paleoIsLoading: true
+      }
+
+      const actual = actions.paleoIsLoading(true);
+
+      expect(actual).toEqual(expected);      
+    });
+  });
 });
+
+// expect(actual).toEqual(expected);
--- 7-day-menu-planner/src/actions/index.js
+++ 7-day-menu-planner/src/actions/index.js
@@ -1,4 +1,10 @@
+// PALEO ACTIONS
 export const fetchPaleoSuccess = (recipes) => ({
   type: 'FETCH_PALEO_SUCCESS',
   recipes
+});
+
+export const paleoIsLoading = (bool) => ({
+  type: 'PALEO_IS_LOADING',
+  paleoIsLoading: bool
 });
