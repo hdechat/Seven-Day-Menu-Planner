699d5e021165626e82fd7b88d34fc72df304a9c0
Helen Dechat <hdechat@gmail.com>
2018-05-28 14:12:39 -0600

    Add rendered image, title, and link to RecipeCard component

--- 7-day-menu-planner/src/components/RecipeCard/index.js
+++ 7-day-menu-planner/src/components/RecipeCard/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 const RecipeCard = ({ recipe }) => {
   return (
     <article>
+      <h1>{recipe.title}</h1>
+      <img src={recipe.image} alt="recipe meal" />
+      <a href={recipe.url}>Click Here to Go To Recipe Page</a>
     </article>
   )
 }
