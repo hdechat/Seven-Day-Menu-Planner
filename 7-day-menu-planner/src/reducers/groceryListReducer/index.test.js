ab6a1356f46033d413fff00d21dd83a8db92bf3e
Helen Dechat <hdechat@gmail.com>
2018-05-28 22:37:38 -0600

    Add groceryListReducer

--- 7-day-menu-planner/src/reducers/groceryListReducer/index.test.js
+++ 7-day-menu-planner/src/reducers/groceryListReducer/index.test.js
@@ -1,7 +1,7 @@
 import * as reducers from './index.js';
 import { addToGroceryList } from '../../actions';
 
-describe('Add To Grocery List', () => {
+describe('Grocery List Reducer', () => {
   let mockState;
 
   beforeEach(() => {
@@ -18,7 +18,7 @@ describe('Add To Grocery List', () => {
     mockState = ['last recipes ingredients']
     const mockIngredients = ["1½ pounds fresh red jalapeño peppers, stemmed, seeded, and roughly chopped", "8 garlic cloves, peeled and smashed", "⅓ cup apple cider vinegar", "3 tablespoons tomato paste", "3 tablespoons honey", "2 tablespoons Paleo-friendly fish sauce (Red Boat!)", "1½ teaspoons kosher salt"];
 
-    const expected [...mockState, ...mockIngredients];
+    const expected = [...mockState, ...mockIngredients];
 
     const actual = reducers.groceryListReducer(mockState, addToGroceryList(mockIngredients));
 
