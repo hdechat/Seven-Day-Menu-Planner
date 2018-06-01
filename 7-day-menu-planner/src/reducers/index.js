4d26492ab4de9890c547f08ecf58910cdbd7acdb
Helen Dechat <hdechat@gmail.com>
2018-05-31 23:53:52 -0600

    Add favorites to rootReducer

--- 7-day-menu-planner/src/reducers/index.js
+++ 7-day-menu-planner/src/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 import recipes from './recipesReducer';
 import menu from './menuReducer';
 import groceryList from './groceryListReducer';
+import favorites from './favoritesReducer';
 
 const rootReducer = combineReducers({
-  recipes, menu, groceryList
+  recipes, menu, groceryList, favorites
 });
 
 export default rootReducer;
