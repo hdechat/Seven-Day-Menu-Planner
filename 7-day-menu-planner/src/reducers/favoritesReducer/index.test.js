61ca76a14e760adcb5cdcfe279fe1775ec693a26
Helen Dechat <hdechat@gmail.com>
2018-05-31 23:48:11 -0600

    Add favoritesReducer test

---
+++ 7-day-menu-planner/src/reducers/favoritesReducer/index.test.js
@@ -0,0 +1,31 @@
+import * as reducers from '.index.js';
+import { addToFavorites } from '../../actions';
+
+describe('Favorites Reducer', () => {
+  let mockState;
+
+  beforeEach(() => {
+    mockState = [];
+  });
+
+  it('should return initialState if there is no given state', () => {
+
+    const actual = reducers.favoritesReducer(undefined, {});
+
+    expect(actual).toEqual(mockState);
+  });
+
+  it('should return an array of recipe objects when it receives the correct action', () => {
+    const mockFavoritedRecipe = {
+      "image": "https://www.edamam.com/web-img/e50/e50584043c015a91a26a7",
+      "title": "Paleo Sriracha recipes",
+      "url": "http://nomnompaleo.com/post/36060636540/paleo-sriracha"
+    }
+
+    const expected = [...mockState, mockFavoritedRecipe];
+
+    const actual = reducers.favoritesReducer(mockState, addToFavorites(mockFavoritedRecipe));
+
+    expect(actual).toEqual(expected);
+  });
+});
