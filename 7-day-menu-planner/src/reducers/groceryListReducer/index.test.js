419ab47489e7bf973b5db201ae2754dc3ba47ef1
Helen Dechat <hdechat@gmail.com>
2018-06-02 17:34:36 -0600

    Add REMOVE_FROM_GROCERY_LIST case to groceryListReducer

--- 7-day-menu-planner/src/reducers/groceryListReducer/index.test.js
+++ 7-day-menu-planner/src/reducers/groceryListReducer/index.test.js
@@ -1,22 +1,14 @@
 import * as reducers from './index.js';
-import { addToGroceryList } from '../../actions';
+import { addToGroceryList, removeFromGroceryList } from '../../actions';
 
 describe('Grocery List Reducer', () => {
   let mockState;
+  let mockIngredients;
 
   beforeEach(() => {
     mockState = [];
-  });
 
-  it('should return initial state if there is no given state', () => {
-    const actual = reducers.groceryListReducer(undefined, {});
-
-    expect(actual).toEqual(mockState);
-  });
-
-  it('should return an array of ingredients when it receives the correct action', () => {
-    mockState = ['last recipes ingredients'];
-    const mockIngredients = [
+    mockIngredients = [
       "1½ pounds fresh red jalapeño peppers, stemmed, seeded, and roughly chopped", 
       "8 garlic cloves, peeled and smashed", 
       "⅓ cup apple cider vinegar", 
@@ -25,11 +17,30 @@ describe('Grocery List Reducer', () => {
       "2 tablespoons Paleo-friendly fish sauce (Red Boat!)", 
       "1½ teaspoons kosher salt"
     ];
+    
+  });
+
+  it('should return initial state if there is no given state', () => {
+    const actual = reducers.groceryListReducer(undefined, {});
+
+    expect(actual).toEqual(mockState);
+  });
+
+  it('should add an array of ingredients when it receives the addToGroceryList action', () => {
+    mockState = ['last recipes ingredients'];
 
     const expected = [...mockState, ...mockIngredients];
 
     const actual = reducers.groceryListReducer(mockState, addToGroceryList(mockIngredients));
 
     expect(actual).toEqual(expected);
+  });
+
+  it('should remove an array of ingredients when it receives the removeFromGroceryList action', () => {
+    mockState = mockIngredients;
+
+    const actual = reducers.groceryListReducer(mockState, removeFromGroceryList(mockIngredients));
+
+    expect(actual).toEqual([]);
   });
 });
