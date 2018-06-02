b0e78138260605f18334f2ada07e6e13627112a7
Helen Dechat <hdechat@gmail.com>
2018-06-02 15:36:20 -0600

    Restructure: change MenuCalendar to receive no props, render DayContainer sending prop of 'day'

--- 7-day-menu-planner/src/components/Day/index.js
+++ 7-day-menu-planner/src/components/Day/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import RecipeCardContainer from '../../containers/RecipeCardContainer';
 import './index.css';
 
-const Day = ({ menu }) => {
+const Day = ({ day, menu }) => {
   return (
     <section className="day">
       <h6>Breakfast</h6>
-      <RecipeCardContainer recipe={menu.breakfast} displayAddToMenu={false} />
+      <RecipeCardContainer recipe={menu[day].breakfast} displayAddToMenu={false} />
 
       <h6>Lunch</h6>
-      <RecipeCardContainer recipe={menu.lunch} displayAddToMenu={false} />
+      <RecipeCardContainer recipe={menu[day].lunch} displayAddToMenu={false} />
 
       <h6>Dinner</h6>
-      <RecipeCardContainer recipe={menu.dinner} displayAddToMenu={false} />
+      <RecipeCardContainer recipe={menu[day].dinner} displayAddToMenu={false} />
     </section>
   );
 };
