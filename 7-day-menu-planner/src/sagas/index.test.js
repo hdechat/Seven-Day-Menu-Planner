bedeb183a1324eaee4c964141d302ee7fe119f0f
Helen Dechat <hdechat@gmail.com>
2018-06-03 22:56:19 -0600

    Update sagas tests

--- 7-day-menu-planner/src/sagas/index.test.js
+++ 7-day-menu-planner/src/sagas/index.test.js
@@ -1,33 +1,11 @@
 import * as sagas from './index.js';
-import { call, put, takeLatest } from 'redux-saga/effects';
-import fetchRecipes from '../api';
-import { fetchRecipesSuccess, recipesIsLoading, recipesHasErrored } from '../actions';
+import { call, put, takeLatest, takeEvery } from 'redux-saga/effects';
+import fetchRecipes, { getDataFromStorage, setFavoritesToStorage } from '../api';
+import { fetchRecipesSuccess, recipesIsLoading, recipesHasErrored, loadFavoritesToStore } from '../actions';
 import { cleanedPaleoRecipes, paleo } from '../mock-data.js';
 
-describe('listenForChooseCategory', () => {
-  let iterator;
 
-  beforeAll(() => {
-    iterator = sagas.listenForChooseCategory();
-  });
-
-  it('should yield takeLatest with the correct params', () => {
-    const value = iterator.next().value;
-
-    const expected = takeLatest('CHOOSE_CATEGORY', sagas.chooseCategory);
-
-    expect(value).toEqual(expected);
-  });
-
-  it('should be done', () => {
-    const done = iterator.next().done;
-
-    expect(done).toBe(true);
-  });
-
-});
-
-describe('chooseCategory happy path', () => {
+describe('chooseCategory', () => {
   let iterator;
   let mockAction;
 
@@ -104,4 +82,203 @@ describe('chooseCategory on error', () => {
     expect(done).toBe(true);
   });
 
+});
+
+describe('addFavoriteToStorage', () => {
+  let iterator;
+  let mockAction;
+
+  beforeAll(() => {
+    mockAction = {
+      type: 'ADD_FAVORITE_TO_STORAGE',
+      recipe: {title: 'recipe name'}
+    };
+
+    iterator = sagas.addFavoriteToStorage(mockAction);
+  });
+
+  it('should yield call with getDataFromStorage and correct params', () => {
+    //Need to learn out how to mock the getDatafromStorage call
+
+    const value = iterator.next().value;
+
+  });
+
+
+  it('should yield call with setFavoritesToStorage and correct params', () => {
+    const mockUpdatedFavorites = [{title: 'recipe name'}]
+
+    const expected = call(setFavoritesToStorage, mockUpdatedFavorites);
+
+    const value = iterator.next().value;
+
+    expect(value).toEqual(expected);
+  });
+
+  it('should be done', () => {
+    const done = iterator.next().done;
+
+    expect(done).toBe(true);
+  });
+});
+
+describe('removeFavoriteFromStorage', () => {
+  let iterator;
+  let mockAction;
+
+  beforeAll(() => {
+    mockAction = {
+      type: 'REMOVE_FAVORITE_FROM_STORAGE',
+      recipe: {title: 'recipe name'}
+    };
+
+    iterator = sagas.removeFavoriteFromStorage(mockAction);
+  });
+
+  it('should yield call with getDataFromStorage and correct params', () => {
+    //Need to learn out how to mock the getDatafromStorage call
+
+    const value = iterator.next().value;
+  });
+
+
+  it('should yield call with setFavoritesToStorage and correct params', () => {
+    const mockUpdatedFavorites = [{title: 'recipe name'}];
+
+    const expected = call(setFavoritesToStorage, []);
+
+    const value = iterator.next(mockUpdatedFavorites).value;
+
+    expect(value).toEqual(expected);
+  });
+
+  it('should be done', () => {
+    const done = iterator.next().done;
+
+    expect(done).toBe(true);
+  });
+});
+
+describe('retrieveDataFromStorage', () => {
+  let iterator;
+  let mockAction;
+
+  beforeAll(() => {
+    mockAction = {
+      type: 'RETRIEVE_DATA_FROM_STORAGE',
+      kdy: 'favorites'
+    };
+
+    iterator = sagas.retrieveDataFromStorage(mockAction);
+  });
+
+  it('should yield call with getDataFromStorage and correct params', () => {
+    //Need to learn out how to mock the getDatafromStorage call
+
+    const value = iterator.next().value;
+  });
+
+
+  it('should yield put with loadFavoritesToStore with correct params', () => {
+    const expected = put(loadFavoritesToStore());
+
+    const value = iterator.next().value;
+
+    expect(value).toEqual(expected);
+  });
+
+  it('should be done', () => {
+    const done = iterator.next().done;
+
+    expect(done).toBe(true);
+  });
+});
+
+//LISTENERS
+describe('listenForChooseCategory', () => {
+  let iterator;
+
+  beforeAll(() => {
+    iterator = sagas.listenForChooseCategory();
+  });
+
+  it('should yield takeLatest with the correct params', () => {
+    const value = iterator.next().value;
+
+    const expected = takeLatest('CHOOSE_CATEGORY', sagas.chooseCategory);
+
+    expect(value).toEqual(expected);
+  });
+
+  it('should be done', () => {
+    const done = iterator.next().done;
+
+    expect(done).toBe(true);
+  });
+});
+
+describe('listenForAddFavoriteToStorage', () => {
+  let iterator;
+
+  beforeAll(() => {
+    iterator = sagas.listenForAddFavoriteToStorage();
+  });
+
+  it('should yield takeEvery with the correct params', () => {
+    const value = iterator.next().value;
+
+    const expected = takeEvery('ADD_FAVORITE_TO_STORAGE', sagas.addFavoriteToStorage);
+
+    expect(value).toEqual(expected);
+  });
+
+  it('should be done', () => {
+    const done = iterator.next().done;
+
+    expect(done).toBe(true);
+  });
+});
+
+describe('listenForRemoveFavoriteFromStorage', () => {
+  let iterator;
+
+  beforeAll(() => {
+    iterator = sagas.listenForRemoveFavoriteFromStorage();
+  });
+
+  it('should yield takeEvery with the correct params', () => {
+    const value = iterator.next().value;
+
+    const expected = takeEvery('REMOVE_FAVORITE_FROM_STORAGE', sagas.removeFavoriteFromStorage);
+
+    expect(value).toEqual(expected);
+  });
+
+  it('should be done', () => {
+    const done = iterator.next().done;
+
+    expect(done).toBe(true);
+  });
+});
+
+describe('listenForRetrieveDataFromStorage', () => {
+  let iterator;
+
+  beforeAll(() => {
+    iterator = sagas.listenForRetrieveDataFromStorage();
+  });
+
+  it('should yield takeEvery with the correct params', () => {
+    const value = iterator.next().value;
+
+    const expected = takeEvery('RETRIEVE_DATA_FROM_STORAGE', sagas.retrieveDataFromStorage);
+
+    expect(value).toEqual(expected);
+  });
+
+  it('should be done', () => {
+    const done = iterator.next().done;
+
+    expect(done).toBe(true);
+  });
 });
