26287ee98de0d16ee53df8956ea5eb70aa34f35c
Helen Dechat <hdechat@gmail.com>
2018-06-04 19:05:46 -0600

    Style grocery list

--- 7-day-menu-planner/src/components/GroceryList/index.js
+++ 7-day-menu-planner/src/components/GroceryList/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './index.css'
 
 const GroceryList = ({ groceryList }) => {
   const groceries = groceryList.map((foodItem, index) => {
     return (
-      <p key={foodItem[0] + foodItem[1] + index}>{foodItem}</p>
+      <li className="food-item" key={foodItem[0] + foodItem[1] + index}><p>{foodItem}</p></li>
     )
   });
   return (
     <section className="grocery-list">
       <h2>Grocery List</h2>
-      { groceries }
+      <ul className="groceries">
+        { groceries }
+      </ul>
     </section>
   )
 }
